4796143447f51236f6d5603246c7e45fc02f52c4
Omar Mohamed <om1963448@gmail.com>
2024-06-23 11:21:32 +0300

    Update GameContents.java

--- GameContents.java
+++ GameContents.java
@@ -1,3 +1,5 @@
+/** this is an interface contain the common data fields in the project */
+
 import java.util.Random;
 
 public interface GameContents {
@@ -5,6 +7,6 @@ public interface GameContents {
        static final int SCREEN_WIDTH = 1300;
        static final int SCREEN_HEIGHT = 750;
        static final int UNIT_SIZE = 22;
-       static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
+       static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE * UNIT_SIZE);    // to separete the screen as a pixels
        Random random = new Random();
 }
