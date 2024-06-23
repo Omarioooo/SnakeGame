63e5f248cecb31f9cb5515e7e4033a5e1030125f
Omar Mohamed <om1963448@gmail.com>
2024-06-23 11:29:48 +0300

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,13 +1,18 @@
-import javax.swing.*;
+/** The swing frame display the game board */\
 
+import javax.swing.*;
 public class GameFrame extends JFrame {
 
-    private final ImageIcon GameIcon ;
+      // An icon for the Game 
+    private final ImageIcon GameIcon ;  
 
     GameFrame(String diff, String mod) {
+        
         GameIcon = new ImageIcon(getClass().getResource("/Icon/images.png"));
         setTitle("Snake game");
         setIconImage(GameIcon.getImage());
+         
+          // The modes of the game play
         if(mod.equals("Mode 1")) {
             add(new GamePanel(diff));
         } else if(mod.equals("Mode 2")) {
@@ -15,7 +20,8 @@ public class GameFrame extends JFrame {
         }else {
             add(new GamePanel3(diff));
         }
-        pack();
+        
+        pack();  // to collect the output from the JPanel page
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);   // the window open in the center of the screen
